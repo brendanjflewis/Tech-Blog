d56471140c15e903448bfafaf0b56eaf13d7b599
brendanjlewis <brendan.j.f.lewis@gmail.com>
2022-02-06 05:15:16 -0500

    added post layout and comment javascript

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -26,8 +26,57 @@ router.get('/', (req, res) => {
         ]
     })
         .then(dbPostData => {
+            const posts = dbPostData.map(post => post.get({ plain: true }));
             // pass a single post object into the homepage template
-            res.render('homepage', dbPostData[0]);
+            res.render('homepage', {
+                posts,
+                loggedIn: req.session.loggedIn
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+router.get('/post/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'post_url',
+            'title',
+            'created_at',
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+        .then(dbPostData => {
+            if (!dbPostData) {
+                res.status(404).json({ message: 'No post found with this id' });
+                return;
+            }
+
+            const post = dbPostData.get({ plain: true });
+
+            res.render('single-post', {
+                post,
+                loggedIn: req.session.loggedIn
+            });
         })
         .catch(err => {
             console.log(err);
