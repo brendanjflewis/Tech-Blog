4291ee6d269213b551a16cb89c5fcd292f87f7d7
brendanjlewis <brendan.j.f.lewis@gmail.com>
2022-02-08 18:20:16 -0500

    attempt to fix login and signup

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User } = require('../../models');
+const { User, Post, Comment } = require('../../models');
 
 // GET all users
 router.get('/', (req, res) => {
@@ -19,7 +19,26 @@ router.get('/:id', (req, res) => {
         attributes: { excludes: ['password'] },
         where: {
             id: req.params.id
-        }
+        },
+        include: [
+            {
+                model: Post,
+                attributes: ['id', 'title', 'post_url', 'created_at']
+            },
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'created_at'],
+                include: {
+                    model: Post,
+                    attributes: ['title']
+                }
+            },
+            {
+                model: Post,
+                attributes: ['title'],
+                as: 'voted_posts'
+            }
+        ]
     })
         .then(dbUserData => {
             if (!dbUserData) {
